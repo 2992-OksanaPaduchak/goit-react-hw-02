530b50eaec741685d20def19b68b8d88e7538dba
Oksana Paduchak <2992paduhcak80@gmail.com>
2025-03-29 00:15:05 +0200

    fix 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import Description from "./components/Description/Description";
 import Options from "./components/Options/Options";
 import Feedback from "./components/Feedback/Feedback";
 import Notification from "./components/Notification/Notification";
-
+import Description from "./components/Description/Description";
 const initialValueFeedbackData = {
   good: 0,
   neutral: 0,
--- src/Components/opch.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const opch = () => {
-  return <div></div>;
-};
-
-export default opch;
