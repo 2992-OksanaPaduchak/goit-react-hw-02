8a801b1f3f29475a50874662b87e259d96a30149
Oksana Paduchak <2992paduhcak80@gmail.com>
2025-03-28 23:24:27 +0200

    homeework 2

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import Description from "./Components/Description/Description";
+import Options from "./Components/Options/Options";
+import Feedback from "./Components/Feedback/Feedback";
+import Notification from "./Components/Notification/Notification";
+
+const initialValueFeedbackData = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
+
+const App = () => {
+  const [feedbackData, setFeedbackData] = useState(() => {
+    const savedFeedback = JSON.parse(localStorage.getItem("feedbackData"));
+    if (savedFeedback) {
+      return savedFeedback;
+    }
+    return initialValueFeedbackData;
+  });
+
+  const totalFeedback =
+    feedbackData.good + feedbackData.bad + feedbackData.neutral;
+  const positiveFeedback = Math.round(
+    (feedbackData.good / totalFeedback) * 100
+  );
+
+  const updateFeedback = (feedbackType) => {
+    setFeedbackData((prevValue) => ({
+      ...prevValue,
+      [feedbackType]: prevValue[feedbackType] + 1,
+    }));
+  };
+
+  const reset = () => {
+    setFeedbackData(initialValueFeedbackData);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("feedbackData", JSON.stringify(feedbackData));
+  }, [feedbackData]);
+
+  return (
+    <>
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        totalFeedback={totalFeedback}
+        reset={reset}
+      />
+      {totalFeedback ? (
+        <Feedback
+          feedbackData={feedbackData}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      ) : (
+        <Notification />
+      )}
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/Notification/Notification.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import s from "./Notification.module.css";
+const Notification = () => {
+  return (
+    <div>
+      <p className={s.text}>No feedback yet</p>
+    </div>
+  );
+};
+
+export default Notification;
